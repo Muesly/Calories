de0501e5c176b430fcaa2645a02b615ec0da8802
Tony Short <tshort15@ford.com>
2023-02-08 21:10:23 +0000

    Creates a panel at top of the screen and splits CaloriesView into subviews

--- Calories/CaloriesView.swift
+++ Calories/CaloriesView.swift
@@ -9,59 +9,19 @@ import SwiftUI
 import CoreData
 
 struct CaloriesView: View {
-    private let viewModel: CaloriesViewModel
-    @Environment(\.scenePhase) var scenePhase
+    @ObservedObject var viewModel: CaloriesViewModel
     @Environment(\.managedObjectContext) private var viewContext
-    @State private var showingAlert = false
-    @State private var totalCaloriesConsumed = 0.0
-    @State private var isShowingFailureToAuthoriseAlert = false
-    @State private var foodDescription: String = ""
-    @State private var calories: Int = 0
-    @State var timeConsumed: Date = Date()
-    @FocusState private var descriptionIsFocused: Bool
-    @FocusState private var caloriesIsFocused: Bool
+    @State var totalCaloriesConsumed: Int = 0
 
     init(viewModel: CaloriesViewModel) {
         self.viewModel = viewModel
     }
 
-    var numberFormatter: NumberFormatter {
-        let formatter = NumberFormatter()
-        formatter.zeroSymbol = ""
-        return formatter
-    }
-
     var body: some View {
         NavigationView {
             VStack {
-                HStack {
-                    TextField("Food", text: $foodDescription)
-                        .focused($descriptionIsFocused)
-                    TextField("Calories", value: $calories, formatter: numberFormatter)
-                        .focused($caloriesIsFocused)
-                        .keyboardType(.numberPad)
-                }
-                DatePicker("Time consumed", selection: $timeConsumed, displayedComponents: .hourAndMinute)
-
-                Button {
-                    Task {
-                        do {
-                            descriptionIsFocused = false
-                            caloriesIsFocused = false
-                            try await viewModel.addFood(foodDescription: foodDescription,
-                                                        calories: calories,
-                                                        timeConsumed: timeConsumed)
-                            foodDescription = ""
-                            calories = 0
-                            totalCaloriesConsumed = try await viewModel.totalCaloriesConsumed()
-                        } catch {
-                            isShowingFailureToAuthoriseAlert = true
-                        }
-                    }
-                } label: {
-                    Text("Add")
-                }
-                Text("\(totalCaloriesConsumed) total calories consumed")
+                HeaderView(totalCaloriesConsumed: $totalCaloriesConsumed)
+                AddEntryView(viewModel: viewModel, totalCaloriesConsumed: $totalCaloriesConsumed)
                 List {
                     ForEach(viewModel.foodEntries) { foodEntry in
                         NavigationLink {
@@ -75,20 +35,6 @@ struct CaloriesView: View {
             }
             .padding()
             .navigationTitle("Calories")
-            .onChange(of: scenePhase) { newPhase in
-                if newPhase == .active {
-                    Task {
-                        foodDescription = ""
-                        calories = 0
-                        timeConsumed = Date()
-                        totalCaloriesConsumed = try await viewModel.totalCaloriesConsumed()
-                    }
-                }
-            }
-            .alert("Failed to access vehicle health",
-                   isPresented: $isShowingFailureToAuthoriseAlert) {
-                Button("OK", role: .cancel) {}
-            }
             Text("Select an item")
         }
     }
@@ -106,6 +52,96 @@ struct CaloriesView: View {
 //                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
 //            }
 //        }
+    }
+}
+
+struct HeaderView: View {
+    @Binding var totalCaloriesConsumed: Int
+
+    var body: some View {
+        HStack(alignment: .top) {
+            VStack(alignment: .leading) {
+                Text("BMR: 1925")
+                Text("Exercise: 350")
+                Text("Combined: 2275")
+            }
+            VStack(alignment: .leading) {
+                Text("Consumption: \(totalCaloriesConsumed)")
+                Text("Difference: -675")
+                Text("Deficit goal: -500")
+                Text("Can eat: 175")
+            }
+        }
+        .padding()
+        .background(Color("mintGreen"))
+        .cornerRadius(10)
+    }
+}
+
+struct AddEntryView: View {
+    private let viewModel: CaloriesViewModel
+    @Environment(\.scenePhase) var scenePhase
+    @State private var foodDescription: String = ""
+    @State private var calories: Int = 0
+    @State var timeConsumed: Date = Date()
+    @FocusState private var descriptionIsFocused: Bool
+    @FocusState private var caloriesIsFocused: Bool
+    @State private var isShowingFailureToAuthoriseAlert = false
+    @Binding var totalCaloriesConsumed: Int
+
+    var numberFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.zeroSymbol = ""
+        return formatter
+    }
+
+    init(viewModel: CaloriesViewModel, totalCaloriesConsumed: Binding<Int>) {
+        self.viewModel = viewModel
+        _totalCaloriesConsumed = totalCaloriesConsumed
+    }
+
+    var body: some View {
+        HStack {
+            TextField("Food", text: $foodDescription)
+                .focused($descriptionIsFocused)
+            TextField("Calories", value: $calories, formatter: numberFormatter)
+                .focused($caloriesIsFocused)
+                .keyboardType(.numberPad)
+        }
+        DatePicker("Time consumed", selection: $timeConsumed, displayedComponents: .hourAndMinute)
+
+        Button {
+            Task {
+                do {
+                    descriptionIsFocused = false
+                    caloriesIsFocused = false
+                    try await viewModel.addFood(foodDescription: foodDescription,
+                                                calories: calories,
+                                                timeConsumed: timeConsumed)
+                    foodDescription = ""
+                    calories = 0
+                    totalCaloriesConsumed = try await viewModel.totalCaloriesConsumed()
+                } catch {
+                    isShowingFailureToAuthoriseAlert = true
+                }
+            }
+        } label: {
+            Text("Add")
+        }
+        .onChange(of: scenePhase) { newPhase in
+            if newPhase == .active {
+                Task {
+                    foodDescription = ""
+                    calories = 0
+                    timeConsumed = Date()
+                    totalCaloriesConsumed = try await viewModel.totalCaloriesConsumed()
+                }
+            }
+        }
+        .alert("Failed to access vehicle health",
+               isPresented: $isShowingFailureToAuthoriseAlert) {
+            Button("OK", role: .cancel) {}
+        }
     }
 }
 
