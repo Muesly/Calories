82ebae386904518bf5e891386cff1b81b8d496e1
Tony Short <tshort15@ford.com>
2023-02-28 20:19:53 +0000

    Adds ability to move between weeks

--- CaloriesWidget/CaloriesWidget.swift
+++ CaloriesWidget/CaloriesWidget.swift
@@ -5,44 +5,66 @@
 //  Created by Tony Short on 19/02/2023.
 //
 
-import WidgetKit
+import HealthKit
 import SwiftUI
+import WidgetKit
 
 struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+    let healthStore: HealthStore
+
+    init(healthStore: HealthStore = HKHealthStore()) {
+        self.healthStore = healthStore
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
+    func placeholder(in context: Context) -> CanEatEntry {
+        CanEatEntry(date: Date(), calories: 0)
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (CanEatEntry) -> ()) {
+        let entry = CanEatEntry(date: Date(), calories: 100)
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
+        var entries: [CanEatEntry] = []
+        let entryDate = Calendar.current.date(byAdding: .hour, value: 1, to: currentDate)!
+        fetchCanEatCalories { canEat in
+            let entry = CanEatEntry(date: entryDate, calories: canEat)
             entries.append(entry)
+            let timeline = Timeline(entries: entries, policy: .atEnd)
+            completion(timeline)
         }
+    }
 
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
+    func fetchCanEatCalories(completion: @escaping ((Int) -> Void)) {
+        let date = Date()
+        Task {
+            do {
+                let bmr = try await healthStore.bmr(date: date)
+                let exercise = try await healthStore.exercise(date: date)
+                let caloriesConsumed = try await healthStore.caloriesConsumed(date: date)
+                let difference = -(caloriesConsumed - (bmr + exercise))
+                completion(difference)
+            } catch {
+                completion(0)
+            }
+        }
     }
 }
 
-struct SimpleEntry: TimelineEntry {
-    let date: Date
+struct CanEatEntry: TimelineEntry {
+    var date: Date
+    let calories: Int
 }
 
 struct CaloriesWidgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        Text(entry.date, style: .time)
+        Text("\(entry.calories)")
     }
 }
 
@@ -54,14 +76,7 @@ struct CaloriesWidget: Widget {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             CaloriesWidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
-    }
-}
-
-struct CaloriesWidget_Previews: PreviewProvider {
-    static var previews: some View {
-        CaloriesWidgetEntryView(entry: SimpleEntry(date: Date()))
-            .previewContext(WidgetPreviewContext(family: .accessoryRectangular))
+        .configurationDisplayName("Can Eat")
+        .description("View how many calories you can eat and still be in deficit according to your goal.")
     }
 }
