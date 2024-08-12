df15bbf44b41f747a28df65c1af87417a3c4a9c2
Tony <tony.short2@gmail.com>
2024-08-12 23:20:35 +0100

    Adds AddExerciseInputFieldsView and saves to database

--- Calories/AddExercise/AddExerciseViewModel.swift
+++ Calories/AddExercise/AddExerciseViewModel.swift
@@ -5,27 +5,33 @@
 //  Created by Tony Short on 16/02/2023.
 //
 
+import CoreData
 import Foundation
 import HealthKit
 import SwiftUI
 
 @Observable
 class AddExerciseViewModel {
+    let container: NSPersistentContainer
     let healthStore: HealthStore
     var suggestions: [Suggestion] = []
     
-    init(healthStore: HealthStore) {
+    init(healthStore: HealthStore,
+         container: NSPersistentContainer = PersistenceController.shared.container) {
         self.healthStore = healthStore
+        self.container = container
     }
     
     func addExercise(exerciseDescription: String, calories: Int, timeExercised: Date) async throws {
-        let exerciseEntry = ExerciseEntry(exerciseDescription: exerciseDescription,
+        try await healthStore.authorize()
+        let exerciseEntry = ExerciseEntry(context: container.viewContext,
+                                          exerciseDescription: exerciseDescription,
                                           calories: calories,
                                           timeExercised: timeExercised)
-        try await healthStore.authorize()
         try await healthStore.addExerciseEntry(exerciseEntry)
+        try container.viewContext.save()
     }
-    
+
     static func shouldClearFields(phase: ScenePhase, date: Date) -> Bool {
         if phase == .active {
             guard let dayDiff = Calendar.current.dateComponents([.day], from: date, to: Date()).day else {
@@ -37,6 +43,27 @@ class AddExerciseViewModel {
     }
     
     func fetchSuggestions(searchText: String = "") {
-        suggestions = [Suggestion(name: "Swimming")]
+        let request = ExerciseEntry.fetchRequest()
+
+        if searchText.isEmpty { // Show list of all previous exercises
+            request.sortDescriptors = [NSSortDescriptor(keyPath: \ExerciseEntry.timeExercised, ascending: false)]
+            guard let results: [ExerciseEntry] = (try? container.viewContext.fetch(request)) else {
+                suggestions = []
+                return
+            }
+            let orderedSet = NSOrderedSet(array: results.map { Suggestion(name: $0.exerciseDescription) })
+            suggestions = orderedSet.map { $0 as! Suggestion }
+        } else {    // Show fuzzy matched strings for this search text
+            request.sortDescriptors = [NSSortDescriptor(keyPath: \ExerciseEntry.timeExercised, ascending: false)]
+            guard let results: [ExerciseEntry] = (try? container.viewContext.fetch(request)) else {
+                suggestions = []
+                return
+            }
+            let filteredResults = results.filter { exerciseEntry in
+                return exerciseEntry.exerciseDescription.fuzzyMatch(searchText)
+            }
+            let orderedSet = NSOrderedSet(array: filteredResults.map { Suggestion(name: $0.exerciseDescription) })
+            suggestions = orderedSet.map { $0 as! Suggestion }
+        }
     }
 }
--- Calories/Model/ExerciseEntry.swift
+++ Calories/Model/ExerciseEntry.swift
@@ -5,10 +5,27 @@
 //  Created by Tony Short on 16/02/2023.
 //
 
+import CoreData
 import Foundation
 
-struct ExerciseEntry {
-    let exerciseDescription: String
-    let calories: Int
-    let timeExercised: Date
+class ExerciseEntry: NSManagedObject {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ExerciseEntry> {
+        return NSFetchRequest<ExerciseEntry>(entityName: "ExerciseEntry")
+    }
+
+    @NSManaged public var exerciseDescription: String
+    @NSManaged public var calories: Int
+    @NSManaged public var timeExercised: Date
+
+    convenience init(context: NSManagedObjectContext,
+                     exerciseDescription: String,
+                     calories: Int,
+                     timeExercised: Date
+    ) {
+        self.init(context: context)
+        self.calories = calories
+        self.exerciseDescription = exerciseDescription
+        self.timeExercised = timeExercised
+    }
 }
